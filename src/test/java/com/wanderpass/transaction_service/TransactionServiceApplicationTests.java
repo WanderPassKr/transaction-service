4a156e23736a73ce54726e789a31dbf827212169
glaxyt <glaxyt@naver.com>
2025-06-28 02:47:50 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/wanderpass/transaction_service/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderpass.transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
